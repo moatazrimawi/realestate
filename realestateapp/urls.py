09fa958c6af06dabb31c3ab1a7b17ed25b84d731
Ola Jaafreh <olajaafreh@Olas-MacBook-Pro.local>
2025-11-08 21:27:23 +0200

    aqdd ola page

--- realestateapp/urls.py
+++ realestateapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from realestateapp import views
 urlpatterns = [
-    path('moataz',views.return_name)
+    path('moataz',views.return_name),
+    path('ola',views.return_name_ola_page)
 ]
--- realestateapp/views.py
+++ realestateapp/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def return_name(request):
     age=36
     return render(request,'moataz.html',{'age':age})
+
+# Create ola page
+def return_name_ola_page(request):
+    age=30
+    name='Ola'
+    context = {'age':age , 'name':name}
+
+    return render(request,'ola.html',context)
